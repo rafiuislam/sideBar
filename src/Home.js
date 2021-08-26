a255dbba5bccd4f72e6e89bdba86753043c3907d
unknown <md.rafiu.islam@g.bracu.ac.bd>
2021-08-26 17:16:18 +0600

    global context added

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,10 @@
 import React, { useContext } from "react";
 import { FaBars } from "react-icons/fa";
+import { useGlobalContext } from "./context";
 
 const Home = () => {
+    const info = useGlobalContext();
+    console.log(info);
     return (
         <main>
             <button className="sidebar-toggle">
